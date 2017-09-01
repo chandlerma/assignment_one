0c0d8c941fdfc9be49c89ed5db7ec419bba17c74
Ma <sm27670@email.vccs.edu>
2017-09-01 15:58:44 -0400

    Initial commit

---
+++ src/assignment1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class assignment1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int bYear;
+        int age;
+        int diff;
+        int year;
+
+        System.out.println("what year is it this year");
+        year = scan.nextInt();
+
+        System.out.println("Please enter your the year that you were born");
+        bYear = scan.nextInt();
+
+        System.out.println("please enter an age");
+        age = scan.nextInt();
+
+        int realAge = year - bYear;
+        diff = age - realAge;
+
+        if( diff == 0 ){
+            System.out.println("congratulations, your birthday is this year.");
+        }else if (diff < 0){
+            diff = diff * -1;
+            System.out.println(" your " + age + "th birthday was " + diff + " year(s) ago.");
+        }else if (diff > 0){
+            System.out.println(" your "+ age + "th birthday will be in " + diff + " years.");
+        }
+
+
+    }
+}
